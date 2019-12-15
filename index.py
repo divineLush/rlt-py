095e1d5bc9a8886804c0ac4a1cfe42e252045b83
Алексей <homeoflucifer@gmail.com>
2019-12-15 17:02:58 +0700

    graphics

--- index.py
+++ index.py
@@ -4,13 +4,25 @@ import random
 
 # TODO: find most occupied trailer tuple
 
+# def gr(target_f_log):
+#     x = np.arange(len(target_f_log))
+#     plt.plot(x, target_f_log)
+#     plt.xlabel(r'$step$')
+#     plt.ylabel(r'$value$')
+#     plt.title(r'$Target Function$')
+#     plt.grid(True)
+#     plt.show()
+
+
 state = S.State()
 prev = state.totalWaitingTime()
+t_f_log = []
 for i in range(1000):
     z = random.randint(0, 2)
     if z == 1:
         state.swapMostWaitingTrailer()
     else :
         state.randSwap()
-    print(state.totalWaitingTime())
-print(prev, state.totalWaitingTime())
+    t_f_log.append(self.totalWaitingTime())
+
+# gr(t_f_log)
